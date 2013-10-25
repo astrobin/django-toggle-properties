7489793df52760679bc19f4e361a6ddb6066f668
Salvatore Iovene <salvatore@iovene.com>
2013-10-25 08:27:31 +0000

    Remove accidentally committed build directory.

--- build/lib.linux-x86_64-2.7/toggleproperties/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-from views import *
-
-urlpatterns = patterns("",
-    url(r'^add$', ajax_add_toggleproperty, name="toggleproperty_ajax_add"),
-    url(r'^remove$', ajax_remove_toggleproperty, name="toggleproperty_ajax_remove"),
-)
