765873034c3a05048cece65b5cd107d73eed1b66
Salvatore Iovene <salvatore@iovene.com>
2019-12-26 23:04:33 +0100

    Fix urls for Django 1.10 compatibility

--- toggleproperties/urls.py
+++ toggleproperties/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from views import *
 
-urlpatterns = patterns("",
+urlpatterns = (
     url(r'^add$', ajax_add_toggleproperty, name="toggleproperty_ajax_add"),
     url(r'^remove$', ajax_remove_toggleproperty, name="toggleproperty_ajax_remove"),
 )
